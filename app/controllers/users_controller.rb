18512d109c4a8b2ca1bb37932779a1426782dffa
jun10jun <tami708j_airuy@yahoo.co.jp>
2020-06-12 09:45:30 +0000

    ユーザー編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,22 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      flash[:success] = '更新しました'
+      redirect_to @user
+    else
+      flash.now[:danger] = "更新できませんでした"
+      render :edit
+    end
+  end
+  
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:show, :new, :create]
+  resources :users, only: [:show, :new, :create, :edit, :update]
 end
