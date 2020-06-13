856fe246f34b64e4177bb8a44976bd6e55b67136
jun10jun <tami708j_airuy@yahoo.co.jp>
2020-06-13 21:48:45 +0000

    ユーザー詳細ページの修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,5 @@
 class ToppagesController < ApplicationController
   def index
-    @items = Item.order(id: :desc).page(params[:page])
+    @items = Item.order(id: :desc).page(params[:page]).per(18)
   end
 end
