21f973a07c9cce616992444bfa080167e7c3a9a3
jun10jun <tami708j_airuy@yahoo.co.jp>
2020-06-21 09:29:32 +0000

    パスワードにバリデーション追加

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,8 @@
 class ToppagesController < ApplicationController
   def index
     @items = Item.order(id: :desc).page(params[:page]).per(18)
+    # @items = Item.order(id: :desc).page(params[:page]).per(18).search(params[:search])
   end
 end
+
+#.where(activated: true).
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,14 @@ class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
   #コメント機能
   has_many :comments
+  
+    # 検索機能
+  # def self.search(search) #ここでのself.はItem.を意味する
+  #   if search
+  #     where(['name LIKE ?', "%#{search}%"]) #検索とitemの部分一致を表示。.は省略
+  #   else
+  #     all #全て表示。Item.は省略
+  #   end
+  # end
+
 end
