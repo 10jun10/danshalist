18adb5f5c337257cdf0284ff3fe5d2d4eec21a0f
jun10jun <tami708j_airuy@yahoo.co.jp>
2020-06-27 02:01:16 +0000

    検索窓の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   
   include SessionsHelper
   
+  # テスト
+  before_action :set_search
+
+  def set_search
+    @search = Item.ransack(params[:q])
+    @items = @search.result.order(updated_at: :desc).page(params[:page]).per(18)
+  end
+  
+  
+  # @search = Item.ransack(params[:q])
+  # @items = @search.result.order(updated_at: :desc).page(params[:page]).per(18)
+  
   private
   
   def require_user_logged_in
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -4,8 +4,8 @@ class ToppagesController < ApplicationController
     # @items = Item.order(id: :desc).page(params[:page]).per(18).search(params[:search])
     # @items = Item.order(updated_at: :desc).page(params[:page]).per(18)
     
-    @search = Item.ransack(params[:q])
-    @items = @search.result.order(updated_at: :desc).page(params[:page]).per(18)
+    # @search = Item.ransack(params[:q])
+    # @items = @search.result.order(updated_at: :desc).page(params[:page]).per(18)
   end
   
   def about
