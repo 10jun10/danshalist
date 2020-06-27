d03690072097aaf162f87c427404aee84a42f940
jun10jun <tami708j_airuy@yahoo.co.jp>
2020-06-27 05:14:45 +0000

    不要部分の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,7 @@ class UsersController < ApplicationController
     #メッセージ
     @currentUserEntry = Entry.where(user_id: current_user.id)
     @userEntry = Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
-    else
+    unless @user.id == current_user.id
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
           if cu.room_id == u.room_id then
